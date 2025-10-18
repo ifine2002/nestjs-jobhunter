d2d694ff7236b3925cfde58d73dc2e77f2179dd1
ifine2002 <boyyhp1@gmail.com>
2025-10-18 13:06:02 +0700

    update user type(JWT)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,8 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { Public } from './decorator/customize';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService, private authService: AuthService) {}
-
-  @Public()
-  @UseGuards(LocalAuthGuard)
-  @Post('/login')
-  login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  @Get('/profile')
-  sayHello(@Request() req) {
-    return req.user;
-  }
 }
