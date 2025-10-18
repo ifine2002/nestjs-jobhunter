bb09a1a5c6e15ea036d3c8d7ab60d106c7aaa1ff
ifine2002 <boyyhp1@gmail.com>
2025-10-18 14:39:05 +0700

    feature(company): query with paginate(use find-isDeleted:false)

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -5,6 +5,7 @@ import { Company, CompanyDocument } from './schemas/company.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { IUser } from 'src/users/users.interface';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class CompaniesService {
@@ -21,8 +22,38 @@ export class CompaniesService {
     });
   }
 
-  findAll() {
-    return `This action returns all companies`;
+  //trả về các document có isDeleted = false -> sử dụng findWithDeleted
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.page;
+    const offset = (currentPage - 1) * limit;
+    const defaultLimit = limit ? limit : 10;
+
+    console.log('>>> check sort: ', sort);
+    const totalItems = (await this.companyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    // if (isEmpty(sort)) {
+    //   // @ts-ignore: Unreachable code error
+    //   sort = '-updatedAt';
+    // }
+
+    const result = await this.companyModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
   }
 
   findOne(id: number) {
