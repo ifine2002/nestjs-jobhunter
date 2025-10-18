5e57c5855aff0939790cbac82330a14b57243a19
ifine2002 <boyyhp1@gmail.com>
2025-10-18 13:15:37 +0700

    passing req.user, update api create company(add createdBy)

--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -1,4 +1,9 @@
-import { SetMetadata } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, SetMetadata } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true); //customize decorator for whilelist AuthGuard('jwt')
+
+export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  return request.user;
+});
