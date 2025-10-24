740f0f12cab23a869d461a582203dbc49a46d14b
ifine2002 <boyyhp1@gmail.com>
2025-10-24 15:39:35 +0700

    config cron job for send email

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { ResponseMessage } from 'src/common/decorators/customize';
 import { MailerService } from '@nestjs-modules/mailer';
+import { Cron } from '@nestjs/schedule';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
   @Get()
+  @Cron('0 10 0 * * 0') // 0h10p am every sunday
   @ResponseMessage('Send email subscriber')
   async handleSendEmail() {
     await this.mailService.sendSubscriberEmail();
